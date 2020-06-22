714452e33082e82082c6b6a5559c7873d338ceb6
yuula <topkashidog@gmail.com>
2020-06-22 15:13:17 +0900

    4 commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color.white,
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('YouTubeアプリ'),
